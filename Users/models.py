1f643945ce7d7e90a133253712ea6915dce6e7bd
Gregr1G <gr.kazak@mail.ru>
2024-07-16 04:45:06 +0300

    models for market

---
+++ Users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Shop(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название магазина')
+    description = models.TextField(blank=True, verbose_name='Описание магазина')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
---
+++ market/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from Users.models import Shop
+class Category(MPTTModel):
+    """Модель описания древовидных категорий"""
+    name = models.CharField(max_length=150, db_index=True, verbose_name='Название категории')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Модель описания продукта"""
+    category = TreeForeignKey(Category, related_name='products', on_delete=models.CASCADE,
+                                 verbose_name='Выберите категорию')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Наменование')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Фото')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Цена')
+    available = models.BooleanField(default=True, verbose_name='Наличие')
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, default="123")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
